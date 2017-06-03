8fd60c051f366e9a1bc7f423d5c7d676a4a97c2e
kirsti <kirstine78@gmail.com>
2017-06-03 19:19:20 +1000

    display first image for food entry in view one country

--- resources/views/country/oneCountry.blade.php
+++ resources/views/country/oneCountry.blade.php
@@ -7,78 +7,68 @@ Version:    1.0
 
 -->
 
-@extends('skeleton')
+@extends('skeleton') @section('content')
 
-@section('content')
+<div class="container">
+	<div class="row">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h2>{{ $oneCountry->name }}</h2>
+			</div>
 
-    <div class="container">
-        <div class="row">
-            <div class="panel panel-default">
-                <div class="panel-heading"><h2>{{ $oneCountry->name }}</h2></div>
+			<div class="panel-body">
 
-                <div class="panel-body">
-
-                    {{ $oneCountry->id }}
-                                          
-                        
-                        @if (count($foodList) > 0)
-		                    <!--  form -->
-		                    <form action="{{ url('#') }}" method="POST" class="form-horizontal">
-		
-		                        <div class="form-group" >
-		                            <div class="col-sm-6">
-		                            
-						                {{ csrf_field() }}
-						                <button type="submit" class="btn btn-neutral" id="btnNewestToOldest" value="newestToOldest" >
-						                    <i class="fa fa-btn fa-trash">New -> Old</i>
-						                </button>						                
-		                            
-						                {{ csrf_field() }}
-						                <button type="submit" class="btn btn-neutral" id="btnOldestToNewest" value="oldestToNewest" >
-						                    <i class="fa fa-btn fa-trash">Old -> New</i>
-						                </button>
-						                
-		                            </div>
-		                        </div>
-		                         <input id="filterOption" type="hidden" name="filterOption" value="newestToOldest">
-		                    </form>
+				{{ $oneCountry->id }} 
+				
+				@if (count($foodList) > 0)
+					<!--  form -->
+					<form action="{{ url('#') }}" method="POST" class="form-horizontal">
+	
+						<div class="form-group">
+							<div class="col-sm-6">
+	
+								{{ csrf_field() }}
+								<button type="submit" class="btn btn-neutral"
+									id="btnNewestToOldest" value="newestToOldest">
+									<i class="fa fa-btn fa-trash">New -> Old</i>
+								</button>
+	
+								{{ csrf_field() }}
+								<button type="submit" class="btn btn-neutral"
+									id="btnOldestToNewest" value="oldestToNewest">
+									<i class="fa fa-btn fa-trash">Old -> New</i>
+								</button>
+	
+							</div>
+						</div>
+						<input id="filterOption" type="hidden" name="filterOption"
+							value="newestToOldest">
+					</form>
 	
 	
-	                        <table class="table table-striped task-table"  id="myTable">
-	
-	                            <!-- Table Body -->
-	                            <tbody>
-	                            @foreach($foodList as $food)
-	                                <tr>
-	                                    <!-- image, show zero or one -->
-	                                    <td class="table-text" width="70%">
-	                                    	@if ($food->images->isNotEmpty())
-	                                        	<img src="{{Storage::disk('public')->url($food->images->first()->filename)}}" alt="{{$food->images->first()->filename}}">
-	                                        @else
-	                                        	<img src="{{URL::asset('/img/not_available.jpg')}}" alt="img not available">
-	                                        @endif
-	                                    </td>
-	                                    
-	                                    <!-- Date -->
-	                                    <td class="table-text" width="30%">
-	                                        <div>{{ $food->date }}</div>
-	                                        <div class='hiddenID' style="display: none;">{{ $food->id }}</div>
-	                                    </td>
-	                                </tr>
-	                            @endforeach
-	                            </tbody>
-	                        </table>
-                        @endif
+					@foreach($foodList as $food)
+						<div class="row">
+							<div class="col-sm-8">
+								@if ($food->images->isNotEmpty()) 
+									<img class="img-responsive" src="{{Storage::disk('public')->url($food->images->first()->filename)}}" alt="{{$food->images->first()->filename}}"> 
+								@else 
+									<img class="img-responsive" src="{{URL::asset('/img/not_available.jpg')}}" alt="img not available"> 
+								@endif
+							</div>
+							<div class="col-sm-4">{{ $food->date }}</div>
+							<div class='hiddenID' style="display: none;">{{ $food->id }}</div>
+							
+						</div>
+						<hr />
+					@endforeach
+				@endif
+			</div>
+		</div>
+	</div>
+</div>
 
-                </div>
-            </div>
-        </div>
-    </div>
-
-@endsection
-
-@section('page-script')
-    <script type="text/javascript">
+@endsection @section('page-script')
+<script type="text/javascript">
 
         $("#btnNewestToOldest").click(function() {
         	$("#filterOption").val("newestToOldest");
@@ -101,5 +91,5 @@ Version:    1.0
 //             window.location.href = url;
 //         });
     </script>
-    
+
 @endsection
