480b7a5654bcf964cb9634ca400f92f04be5148b
kirsti <kirstine78@gmail.com>
2017-08-20 14:36:59 +1000

    add 3 images for About page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -35,6 +35,20 @@ Section 1.10.33 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
 "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
 			</div>
 		</div>
+		
+
+        <div class="row">                
+	        <div class="col-xs-12 col-sm-4 aboutImagePaddingTop">
+				<img class="img-responsive imageHundredPercentWidth" src="{{URL::asset('/img/no_image_available.jpeg')}}" alt="img not available">	        	
+			</div>              
+	        <div class="col-xs-12 col-sm-4 aboutImagePaddingTop">
+				<img class="img-responsive imageHundredPercentWidth" src="{{URL::asset('/img/no_image_available.jpeg')}}" alt="img not available">	 	        	
+			</div>              
+	        <div class="col-xs-12 col-sm-4 aboutImagePaddingTop">
+				<img class="img-responsive imageHundredPercentWidth" src="{{URL::asset('/img/no_image_available.jpeg')}}" alt="img not available">	 	        	
+			</div>
+		</div>
+		
     </div>
 
 @endsection
