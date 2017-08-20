e077e5fba309d17bd0f1c3376c0baedfbaf1e4e7
kirsti <kirstine78@gmail.com>
2017-08-20 14:46:30 +1000

    add Contact Us col on About page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -19,7 +19,7 @@ Version:    1.0
         </div>
 
         <div class="row">                
-	        <div class="col-xs-12 col-sm-12">
+	        <div class="col-xs-12 col-sm-8">
 	        	"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
@@ -33,6 +33,10 @@ Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
 Section 1.10.33 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
 
 "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+			</div>
+	        <div class="col-xs-12 col-sm-4" id="contactUsCol">
+	        	<h5><strong>Contact OmniFood</strong></h5>
+	        	kirstine78@yahoo.com
 			</div>
 		</div>
 		
